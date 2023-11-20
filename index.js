f76bf7f0d57736a1b581b7ab87c97c70ff850692
MaksymKurbesov <bonyklade@gmail.com>
2023-11-20 14:07:39 +0200

    added send email route

--- index.js
+++ index.js
@@ -30,6 +30,23 @@ app.use(express.json());
 
 app.options('*', cors())
 
+app.post("/sendEmail", async (req, res) => {
+	const { to, subject, html } = req.body;
+
+	try {
+		await mailTransport.sendMail({
+			from: `Support <${supportEmail}>`,
+			to,
+			subject,
+			html,
+		});
+		res.status(200).send("Email sent successfully");
+	} catch (error) {
+		console.error("Error sending email:", error);
+		res.status(500).send("Failed to send email");
+	}
+});
+
 app.post('/register', async (req, res) => {
 	const userData = await db.collection('users').doc('admin').get();
 	let userAgentInfo = req.useragent;
@@ -48,7 +65,6 @@ app.post('/register', async (req, res) => {
 		console.error("Error sending email:", error);
 		res.status(500).send("Failed to send email");
 	}
-
 });
 
 app.listen(3333, () => {
